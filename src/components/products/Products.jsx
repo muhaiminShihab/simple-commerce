af2119403832b751e0920aded2042bd0f259b0be
Muhaimin Shihab <talk.shihab@gmail.com>
2022-07-06 17:27:48 +0600

    sceleton effect added

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -15,8 +15,26 @@ function Products() {
             <div className="container">
                 <div className="row">
                     {
+                        (products.length > 0) ?
                         products.map(item =>
                             <ProductCard photo={item.image} title={item.title} price={item.price} id={item.id} key={item.id} />
+                        )
+
+                        :
+                        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item =>
+                            <div className="col-6 col-lg-3">
+                                <div className="product-card sceleton">
+                                    <div className="product-image"></div>
+                                    <div className="product-data">
+                                        <div className="dummy"></div>
+                                        <div className="dummy"></div>
+
+                                        <div className="mt-4 text-end">
+                                            <div className='btn-cart'></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         )
                     }
                 </div>
